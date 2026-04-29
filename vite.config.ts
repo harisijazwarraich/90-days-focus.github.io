8c1a5eee289863a8123a8a741697234afabd71b4
Haris Ijaz <journeyslide@gmail.com>
2026-04-30 02:00:26 +0500

    chore: update base URL in vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/90-days-focus",
+  base: "/90-days-focus.github.io/",
+
 })
